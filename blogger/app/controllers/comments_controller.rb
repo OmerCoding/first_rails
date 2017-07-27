3c0efb159f383a91b60a9a421f10b575c6bade15
Omer Hefets <omerh127@gmail.com>
2017-07-27 23:10:57 +0300

    added the tagging feature

--- blogger/app/controllers/comments_controller.rb
+++ blogger/app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
     @comment.save
+    flash.notice = "A Comment by '#{@comment.author_name}' Posted!"
     redirect_to article_path(@comment.article)
   end
 end
